cbbec6a64cf1cbae6f00b158d2cdd645b3579384
Kozhevnikov Ivan <kozhevnikovivan86@gmail.com>
2023-04-24 13:51:49 +0300

    Find a node and produce a utils function fix-2

--- src/js/utils/render.js
+++ src/js/utils/render.js
@@ -1,6 +1,7 @@
 const createElement = (template) => {
-    const element = document.createElement(template.content);
-    return element;
+    const element = document.createElement('div');
+    element.innerHTML = template;
+    return element.firstChild;
 };
 
 export default createElement;
