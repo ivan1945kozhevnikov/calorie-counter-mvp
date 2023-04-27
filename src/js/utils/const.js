ccf42708bd6d4eb99c38119241a233c37ddfa2ba
Kozhevnikov Ivan <kozhevnikovivan86@gmail.com>
2023-04-27 13:36:12 +0300

    Add file const

---
+++ src/js/utils/const.js
@@ -0,0 +1,16 @@
+const genderInputs = [
+    {
+        id: 'gender-male',
+        value: 'male',
+        checked: true,
+        label: 'Мужчина',
+    },
+    {
+        id: 'gender-female',
+        value: 'female',
+        checked: false,
+        label: 'Женщина',
+    },
+];
+
+export default genderInputs;
